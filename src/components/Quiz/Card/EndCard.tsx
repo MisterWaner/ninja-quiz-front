10ececcf948463bbea096082efc9f200fd17d318
Erwan Soulas <erwan.soulas@gmail.com>
2025-05-21 23:45:32 +0200

    5th commit: --FIXED issue with data fetching in themeselector. --Goes on with gaming ui.

---
+++ src/components/Quiz/Card/EndCard.tsx
@@ -0,0 +1,30 @@
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent,
+    CardFooter,
+} from '@/components/ui/card';
+
+import { useQuizStore } from '@/store/quiz-store';
+
+export default function EndCard() {
+    const score = useQuizStore((state) => state.score);
+
+    return (
+        <Card className='md:w-1/2 mx-auto mt-24'>
+            <CardHeader>
+                <CardTitle className='text-center text-lg uppercase'>
+                    Fin !
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p className='font-bold'>
+                    Bravo ! Tu as terminé le quiz, ton score est de {score} / 10
+                    !
+                </p>
+            </CardContent>
+            <CardFooter className='justify-end'>coucou</CardFooter>
+        </Card>
+    );
+}
